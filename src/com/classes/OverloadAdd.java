fdbdae35618fe52c7e6eaa2002a0507421fe32ce
Meena Kabduwal <kabduwalmeena@gmail.com>
2021-03-10 23:07:55 +0100

    overloaded class with addition method

--- src/com/classes/OverloadAdd.java
+++ src/com/classes/OverloadAdd.java
@@ -1,5 +1,16 @@
 package com.classes;
 
 public class OverloadAdd {
-
+	public int add(int value1, int value2) {
+		return value1 + value2;
+	}
+	public double add(double value1, double value2) {
+		return value1 + value2;
+	}
+	public float add(float value1, float value2) {
+		return value1 + value2;
+	}
+	public long add(long value1, long value2) {
+		return value1 + value2;
+	}
 }

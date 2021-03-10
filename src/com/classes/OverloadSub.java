27c7c8c4cc5e48568fe6183651d6087df2f4760b
Meena Kabduwal <kabduwalmeena@gmail.com>
2021-03-10 23:14:00 +0100

    overloaded class with subtraction method

--- src/com/classes/OverloadSub.java
+++ src/com/classes/OverloadSub.java
@@ -1,5 +1,16 @@
 package com.classes;
 
 public class OverloadSub {
-
+	public int sub(int value1, int value2) {
+		return value1 - value2;
+	}
+	public double sub(double value1, double value2) {
+		return value1 - value2;
+	}
+	public float sub(float value1, float value2) {
+		return value1 - value2;
+	}
+	public long sub(long value1, long value2) {
+		return value1 - value2;
+	}
 }
